4ee1c8f680b1235d340ac325c740a689f53bec84
kamran <kamran@yahoo.com>
2012-12-25 17:24:32 -0500

    add menu files

---
+++ protected/views/site/about.php
@@ -0,0 +1,15 @@
+<?php
+/* @var $this SiteController */
+/* @var $model ContactForm */
+/* @var $form CActiveForm */
+
+$this->pageTitle='About Us';
+$this->breadcrumbs=array(
+	'About',
+);
+?>
+
+<h1>About Us</h1>
+
+Information about company comes here.
+
