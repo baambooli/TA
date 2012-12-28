b443a81aa468e5d51bb9df6acccf27161181501b
kamran <kamran@yahoo.com>
2012-12-28 12:22:09 -0500

    small bug fix

--- protected/models/User.php
+++ protected/models/User.php
@@ -137,6 +137,7 @@ class User extends TaActiveRecord
     {
         $salt = Yii::app()->params['salt'];
         // create a 64 characters hash
-        return md5($salt.$value).md5($salt.$value); 
+        $hash = md5($salt.$value).md5($value.$salt); 
+        return $hash;
     }
 }
