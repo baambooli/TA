92940a45fa9df1678c58b125fad1149f04468cb4
kamran <kamran@yahoo.com>
2013-01-01 14:04:42 -0500

    adjust the css to extend the screen

--- protected/components/AES.php
+++ protected/components/AES.php
@@ -22,7 +22,7 @@ class AES
             $key = hash('SHA256', $key, true);
         $padding = 16 - (strlen($data) % 16);
         $data .= str_repeat(chr($padding), $padding);
-        return base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $data, MCRYPT_MODE_CBC, str_repeat("\0", 16)));
+        return base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $data, MCRYPT_MODE_CBC, str_repeat("\0", 16)));
     }
 
     public static function aes128Decrypt($key, $data)
@@ -39,7 +39,7 @@ class AES
         $data = base64_decode($data);
         if (32 !== strlen($key))
             $key = hash('SHA256', $key, true);
-        $data = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $data, MCRYPT_MODE_CBC, str_repeat("\0", 16));
+        $data = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $data, MCRYPT_MODE_CBC, str_repeat("\0", 16));
         $padding = ord($data[strlen($data) - 1]);
         return substr($data, 0, -$padding);
     }
